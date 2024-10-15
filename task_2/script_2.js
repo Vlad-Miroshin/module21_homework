723c9ce5dfacab1a155bfb2328f88e9169cee6b4
Vlad Miroshin <ppvlad@mail.ru>
2024-10-15 20:11:15 +0300

    feat (task_2) create realization

--- task_2/script_2.js
+++ task_2/script_2.js
@@ -1,48 +1,29 @@
-// Задание 2
-// Напишите функцию, которая принимает в качестве аргументов строку и объект, 
-// а затем проверяет, есть ли у переданного объекта свойство с данным именем. 
-// Функция должна возвращать true или false.
+/*
 
-function hasOwnProp(propName,  obj) {
-    return Object.hasOwn(obj, propName);
-}
+Задание 2
 
-// на всякий случай функция, определяющая наличие свойства
-// по всей цепочке прототипов
+Дан образец JSON-строки.
+Задача — создать JS-объект, который при преобразовании в JSON будет возвращать в качестве результата такую же JSON-строку, как в образце. 
+Получившуюся строку вывести в консоль.
 
-function hasProp(propName,  obj) {
-    return propName in obj;
-}
+{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}
+
+*/
+
+const sample = '{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}';
+
+// Решение
+
+const obj = {
+    name: "Anton",
+    age: 36,
+    skills: ["Javascript", "HTML", "CSS"],
+    salary: 80_000
+};
 
 // проверка
 
-function Customer() {
-    this.uuid = 'f227c5f-de3e-4cc7-b561-32a49a25b210',
-    this.familyName = 'Петров',
-    this.surname = 'Сергей',
-    this.patronymic = 'Павлович',
-    this.discount = 5,
-    this.category = 'Private',
-    this.birthDay = '1983-05-11'
-}
+const jsonText = JSON.stringify(obj);
 
-const customer = new Customer();
-
-console.log('Object customer');
-Object.getOwnPropertyNames(customer).forEach(
-    (key) => console.log(`${key} : ${customer[key]}`)
-);
-
-// добавим свойство прототипу
-Customer.prototype.abc = "abc";
-
-console.log('');
-console.log(`hasOwnProp('familyName', customer) === ${hasOwnProp('familyName', customer)}`);
-console.log(`hasOwnProp('abcdef', customer) === ${hasOwnProp('abcdef', customer)}`);
-
-// первая функция не видит добавленное прототипу свойство
-console.log(`hasOwnProp('abc', customer) === ${hasOwnProp('abc', customer)}`);
-// а вторая видит
-console.log(`hasProp('abc', customer) === ${hasProp('abc', customer)}`);
-
-console.log(`hasProp('zzz', customer) === ${hasProp('zzz', customer)}`);
+console.log(jsonText);
+console.log(jsonText === sample ? 'Удовлетворяет требованиям' : 'fail');
